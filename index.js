efc9a4a796fe13dd2e0a7dfc4efde03c8e60b58c
Alexis Davidson <alexisdavidson00@gmail.com>
2022-11-10 15:20:21 +0800

    First working example

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -12,9 +14,9 @@ const main = async() => {
     //     max_tokens: 6,
     //   });
     const response = await openai.createImage({
-        prompt: "a white siamese cat",
+        prompt: "an impatient web designer",
         n: 1,
-        size: "1024x1024",
+        size: "512x512",
     });
     image_url = response.data.data[0].url;
     console.log(image_url)
